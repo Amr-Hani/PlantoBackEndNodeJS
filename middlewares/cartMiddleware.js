f7a77f82085f2dac1acb519294d4c89d7c0c2bf9
AmrHani <117240613+Amr-Hani@users.noreply.github.com>
2025-03-11 15:46:17 +0200

    fix cart middleware

--- middlewares/cartMiddleware.js
+++ middlewares/cartMiddleware.js
@@ -4,10 +4,7 @@ const cartMiddleware = () => {
   return [
     body("product_id").notEmpty().withMessage("Product ID is required"),
 
-    body("quantity")
-      .optional()
-      .isInt({ min: 1 })
-      .withMessage("Minimum quantity is 1"),
+    body("quantity").optional().isInt().withMessage("Minimum quantity is 1"),
   ];
 };
 
